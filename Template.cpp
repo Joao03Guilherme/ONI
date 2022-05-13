f419a46291efddf3129848cab3d4ecb90f88c09a
João Guilherme <63371307+Joao03Guilherme@users.noreply.github.com>
2022-05-13 22:54:29 +0100

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,24 @@
+# include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+# define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+# define pb push_back
+# define ll long long int
+# define vi vector<int>
+# define pii pair<int, int>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+const int INF = 1000000009;
+const long long INFLL = (ll)INF * (ll)INF;
+
+// Use the pair <int, int> trick to store multiple values
+typedef tree<pair<int, int>, null_type, greater<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+typedef tree<int, null_type, greater<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+int main() {
+	IOS;
+	return 0;
+}
